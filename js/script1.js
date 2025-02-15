a30b680f237fedd534640d599d107a642f6f9ef1
jiwon-ooooa <jiwon0173@hotmail.co.kr>
2025-02-15 18:04:40 +0900

    Delete js directory

--- js/script1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cursor = document.querySelector('.cursor');
-
-    document.addEventListener('mousemove', (event) => {
-        cursor.style.left = `${event.pageX}px`;
-        cursor.style.top = `${event.pageY}px`;
-    });
-});
